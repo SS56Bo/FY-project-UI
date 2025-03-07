dac86d76e39feeb270ff4bd4517e794cecc10f8e
SS56Bo <sohamsarkar560@gmail.com>
2025-03-07 16:42:15 +0530

    Server restructure

--- server/server.go
+++ server/server.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
+
+type UserSignIn struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
 
 func main() {
 	app := fiber.New()
@@ -11,12 +19,28 @@ func main() {
 
 	app.Static("/", "../client/dist")
 
+	//Post Request
+	app.Post("/signin", SignIn)
+
 	app.Get("*", func (c *fiber.Ctx) error {
-		return c.SendString("../client/dist/index.html")
+		return c.SendFile("../client/dist/index.html")
 	})
 
 	app.Listen(":3000")
 }
 
+func SignIn(c *fiber.Ctx) error {
+	info := new(UserSignIn)
+	if err := c.BodyParser(info); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid Request",
+		})
+	}
 
+	fmt.Println("Received response: ", info.UserName, info.Password)
+
+	return c.JSON(fiber.Map{
+		"message": "Sign-in successful",
+	})
+}
 
